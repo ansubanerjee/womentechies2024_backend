feb6f855a391794cdc34127aaa69db54f38fe49c
Ansu <banner1891000@gmail.com>
2024-03-09 10:44:48 +0530

    Mini Changes 1

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+
+//IMPORTS
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
 
+
+
+//SCHEMA
 const Women = require('./models/women');
 const User_category = require('./models/user_category');
 const User = require('./models/user');
 
-const cors = require('cors');
+//GENERAL
 require('dotenv/config');
+app.use(cors());
+const api = process.env.API_URL;
+
+//MIDDLEWARE
+
+//ROUTERS
+
 
 
 const db_url = process.env.db_url;
@@ -20,3 +35,8 @@ mongoose.connect(db_url, connectionParams).then(()=>{
 }).catch((e)=>{
     console.log("Error: ", e)
 });
+
+app.listen(3000, ()=>{
+    console.log(api);
+    console.log("Running at http://localhost:3000");
+})
